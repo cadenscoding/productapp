77bec6675ae79943aba95524f41fb2ae122d928d
Caden F <cadenf@anniecannonss-MacBook-Pro.local>
2025-04-25 14:23:43 -0400

    fixed path

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -8,7 +8,6 @@ function App() {
   return (
     <Router>
       <Routes>
-        {/* Layout wraps all routes */}
         <Route path="/" element={<Layout />}>
           <Route index element={<FeedbackPage />} />
           <Route path="add-feedback" element={<AddFeedbackForm />} />
